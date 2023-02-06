f1037c24e6d929647a5dec1c70067b3f10d88094
locvb <locvb.it@gmail.com>
2023-02-07 00:20:18 +0700

    add using jasypt to encrypt sensitive data

---
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -0,0 +1,16 @@
+package jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setPassword("Demo_Pwd!2020");
+        standardPBEStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = standardPBEStringEncryptor.encrypt("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");
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
+    }
+}
